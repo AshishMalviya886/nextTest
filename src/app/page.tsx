a87ede3b61a873b6c29ea66a85fbffccbfa3c4fb
murtuzakapadia121 <murtuza.kapadia121@gmail.com>
2024-10-30 16:43:28 +0530

    intial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import ResultsSummary from "@/components/results-summary";
+ 
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <ResultsSummary />
+    </main>
+  );
+}
